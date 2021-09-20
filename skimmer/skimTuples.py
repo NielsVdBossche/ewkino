69450c7e6191661fc2daccd6bbe6e2f62a405e57
njovdnbo <niels.vandenbossche@ugent.be>
2021-09-20 16:50:42 +0200

    skimtuples now works in writing to pnfs

--- skimmer/skimTuples.py
+++ skimmer/skimTuples.py
@@ -89,13 +89,20 @@ if __name__ == '__main__' :
                     script.write( skim_command )
 
                 script.write("\n\n")
-                script.write("array=($(ls -p | grep -v /))\n")
-                script.write('for i in "${{array[@]}}"\n')
+                script.write("array=($(ls -p $TMPDIR | grep -v /))\n")
+                script.write('for i in "${array[@]}"\n')
                 script.write("do\n")
                 script.write("\t")
-                script.write('gfal-copy file://$TMPDIR/"$i" srm://maite.iihe.ac.be:8443{}\n'.format(output_directory))
+		script.write('echo "$i"\n')
+		script.write("\t")
+                script.write('gfal-copy file:///$TMPDIR/"$i" srm://maite.iihe.ac.be:8443{}/\n'.format(output_directory))
                 script.write("done\n")
                 #script.write("")
                 #script.write("gfal-copy file://$TMPDIR/*.root srm://maite.iihe.ac.be:8443{}".format(output_directory))
             #submit job and catch errors 
             submitQsubJob( script_name, wall_time )
+
+            #break
+
+        #break
+
