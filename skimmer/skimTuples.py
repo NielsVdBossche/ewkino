cdd8bb289208810e299fbc70c8bef68460a0ba02
njovdnbo <nielsvdbossche@gmail.com>
2021-10-27 10:41:40 +0200

    added extra option in skimtuples to specify a processType

--- skimmer/skimTuples.py
+++ skimmer/skimTuples.py
@@ -47,6 +47,7 @@ if __name__ == '__main__' :
     skim_condition = sys.argv[4]
     files_per_job = int(sys.argv[5]) if len( sys.argv ) >= 6 else 50
     wall_time = sys.argv[6] if len( sys.argv ) >= 7 else '24:00:00' 
+    processes = sys.argv[7] if len( sys.argv ) >= 8 else ''
     if len( sys.argv ) <= 4:
         print('Error: skimTuples.py requires additional command-line arguments.')
         print('Usage: python skimTuples.py < input_directory > < ntuple_version > < output_directory > < skim_condition > < files_per_job > < wall_time >')
@@ -57,6 +58,9 @@ if __name__ == '__main__' :
     sample_directories = []
     sample_sub_directories = []
     for sample_directory, subdirectory in listSampleDirectories( input_directory, version_name ):
+        if processes != '' and not processes in sample_directory: 
+            continue
+
         sample_directories.append( sample_directory )
         sample_sub_directories.append( subdirectory )
     
@@ -91,7 +95,7 @@ if __name__ == '__main__' :
                     script.write( skim_command )
 
                     ### REMOVE LINE FOR NON-DESTRUCTIVE SKIMMING
-                    rmCommands.append('gfal-rm srm://maite.iihe.ac.be:8443{}\n'.format(f))
+                    #rmCommands.append('gfal-rm srm://maite.iihe.ac.be:8443{}\n'.format(f))
 
                 script.write("\n\n")
                 script.write("array=($(ls -p $TMPDIR | grep -v /))\n")
@@ -104,8 +108,8 @@ if __name__ == '__main__' :
                 script.write("done\n")
 
                 ### REMOVE LINE FOR NON-DESTRUCTIVE SKIMMING
-                for command in rmCommands:
-                    script.write(command)
+                #for command in rmCommands:
+                #    script.write(command)
                 #script.write("")
                 #script.write("gfal-copy file://$TMPDIR/*.root srm://maite.iihe.ac.be:8443{}".format(output_directory))
             #submit job and catch errors 
