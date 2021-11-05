1550265e67822ff6648543eb8afd6350ed1d135a
njovdnbo <niels.vandenbossche@ugent.be>
2021-11-05 15:51:03 +0100

    save skimming procedureÃ

--- skimmer/skimTuplesCurrent.sh
+++ skimmer/skimTuplesCurrent.sh
@@ -1,1 +1,4 @@
-python skimTuples.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpreVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 150 24:00:00
+python skimTuples.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpostVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 24:00:00 TTToSemi
+python skimTuples.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2018_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 24:00:00 TTToSemi
+python skimTuples.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2017_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 24:00:00 TTToSemi
+
