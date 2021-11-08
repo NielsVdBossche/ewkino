6d441cd199424e508f236a15952f5ada03786548
njovdnbo <nielsvdbossche@gmail.com>
2021-11-08 11:48:55 +0100

    slight oversight again

--- _FourTopAnalysis/histogramSetups/histogramFillers.cc
+++ _FourTopAnalysis/histogramSetups/histogramFillers.cc
@@ -226,7 +226,7 @@ std::vector<double> fourTopHists::fillAllHists(bool multilep, EventFourT* selec,
         jets->at(0)->deepFlavor(),
         jets->at(1)->deepFlavor(),
         jets->at(2)->deepFlavor(),
-        jets->at(3)->deepFlavor(),
+        (jets->size() > 3 ? jets->at(3)->deepFlavor() : -1.)
 
     };
 
@@ -235,7 +235,7 @@ std::vector<double> fourTopHists::fillAllHists(bool multilep, EventFourT* selec,
     fillVal.push_back(jets->at(0)->deepFlavor());
     fillVal.push_back(jets->at(1)->deepFlavor());
     fillVal.push_back(jets->at(2)->deepFlavor());
-    fillVal.push_back(jets->at(3)->deepFlavor());
+    (jets->size() > 3 ? fillVal.push_back(jets->at(3)->deepFlavor()) : -1.);
 
 
     if (multilep) {
