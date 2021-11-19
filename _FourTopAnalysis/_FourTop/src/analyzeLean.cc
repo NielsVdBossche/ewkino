28ab00cb551cb55216371763c4dd36fc4e89d5f5
njovdnbo <nielsvdbossche@gmail.com>
2021-11-19 11:53:31 +0100

    added mva stuff to analyze lean

--- _FourTopAnalysis/_FourTop/src/analyzeLean.cc
+++ _FourTopAnalysis/_FourTop/src/analyzeLean.cc
@@ -10,14 +10,29 @@ void FourTop:: analyzeLean() {
     std::string channelDL = "DL";
     std::vector<HistInfo>* infoDL = fourTopHists::allHists(channelDL, false, false);
     HistogramManager* DLManager = new HistogramManager(channelDL, infoDL);
+    size_t dlPosMVA = infoDL->size();
+    DLManager->extendHistInfo(mva_DL->createHistograms());
+    DLManager->set2DHistInfo(mva_DL->create2DHistograms());
+
+    dlPosMVA += mva_DL->getMaxClass();
 
     std::string channel3L = "3L";
     std::vector<HistInfo>* info3L = fourTopHists::allHists(channel3L, true, false);
     HistogramManager* TriLManager = new HistogramManager(channel3L, info3L);
+    size_t mlPosMVA = info3L->size();
+    TriLManager->extendHistInfo(mva_ML->createHistograms());
+    TriLManager->set2DHistInfo(mva_ML->create2DHistograms());
+
+    mlPosMVA += mva_ML->getMaxClass();
 
     std::string channel4L = "4L";
     std::vector<HistInfo>* info4L = fourTopHists::allHists(channel4L, true, true);
     HistogramManager* FourLManager = new HistogramManager(channel4L, info4L);
+    size_t fourlPosMVA = info4L->size();
+    FourLManager->extendHistInfo(mva_ML->createHistograms(true));
+    FourLManager->set2DHistInfo(mva_ML->create2DHistograms(true));
+
+    fourlPosMVA += mva_ML->getMaxClass();
 
     std::string channelCRZ = "CRZ";
     std::vector<HistInfo>* infoCRZ = fourTopHists::allHists(channelCRZ, false, false);
@@ -50,6 +65,8 @@ void FourTop:: analyzeLean() {
             // Initialize event
             currentEvent = treeReader->buildEventPtr( entry );
 
+            if (! eventPassesTriggers()) continue;
+
             // Check triggers here
             selection->addNewEvent(currentEvent);
 
@@ -58,35 +75,31 @@ void FourTop:: analyzeLean() {
 
             // apply baseline selection
             // Adapted to work with increased nonprompt yields
-            if (selection->numberOfLeps() < 2) continue; // atm we check our tight leps here, for nonprompt est, this becomes FO
-            if (selection->numberOfLeps() == 2 && selection->getMediumLepCol()->hasOSPair()) continue;
+            //if (selection->numberOfLeps() < 2) continue; // atm we check our tight leps here, for nonprompt est, this becomes FO
+            //if (selection->numberOfLeps() == 2 && selection->getMediumLepCol()->hasOSPair()) continue;
+//
+            //if (selection->numberOfJets() < 2 || selection->numberOfMediumBJets() < 1) continue;
+            //if (selection->getJetCol()->scalarPtSum() < 100) continue;
+//
+            //if (selection->numberOfLeps() == 3 && selection->getJetCol()->scalarPtSum() < 200) continue;
+            //if (selection->numberOfLeps() == 2 && selection->getJetCol()->scalarPtSum() < 300) continue;
+            //if (selection->numberOfLeps() < 4 && selection->numberOfJets() < 3) continue;
+            //if (selection->numberOfLeps() == 2 && selection->numberOfJets() < 4) continue;
 
-            if (selection->numberOfJets() < 2 || selection->numberOfMediumBJets() < 1) continue;
-            if (selection->getJetCol()->scalarPtSum() < 100) continue;
-
-            if (selection->numberOfLeps() == 3 && selection->getJetCol()->scalarPtSum() < 200) continue;
-            if (selection->numberOfLeps() == 2 && selection->getJetCol()->scalarPtSum() < 300) continue;
-            if (selection->numberOfLeps() < 4 && selection->numberOfJets() < 3) continue;
-            if (selection->numberOfLeps() == 2 && selection->numberOfJets() < 4) continue;
-
-
+            if (! selection->passLeanSelection()) continue;
 
             // Apply scale factors
             // TODO
             
             // Basic non-prompt handling (using MC to estimate the contribution):
-            size_t fillIndex = sampleIndex;
             std::vector<double> fillVec;
-            
+    
             // If nonprompt: fillIndex becomes index of nonprompt histograms
-            LeptonCollection* lepCol = selection->getMediumLepCol();
+            // LeptonCollection* lepCol = selection->getMediumLepCol();
             bool nonPrompt = false;
-
-            for (const auto& leptonPtr : *lepCol) {
+            for (const auto& leptonPtr : *selection->getMediumLepCol()) {
                 if (! leptonPtr->isPrompt()) {
-                    fillIndex = treeReader->numberOfSamples();
                     nonPrompt = true;
-
                     break;
                 }
             }
@@ -107,14 +120,47 @@ void FourTop:: analyzeLean() {
 
             // Fill histograms
             if (selection->numberOfLeps() == 2) {
+                std::vector<double> scores = mva_DL->scoreEvent();
+
                 fillVec = fourTopHists::fillAllHists(false, selection);
+                fillVec.insert(fillVec.end(), scores.begin(), scores.end());
+
                 DLManager->fillHistograms(fillVec, currentEvent->weight(), nonPrompt);
+                
+                std::vector<std::pair<double, double>> fillVec2D = mva_DL->fill2DVector();
+                DLManager->fill2DHistograms(fillVec2D, currentEvent->weight(), nonPrompt);
+
+                std::pair<MVAClasses, double> classAndScore = mva_DL->getClassAndScore();                
+                DLManager->fillSingleHistogram(dlPosMVA + classAndScore.first, classAndScore.second, currentEvent->weight(), nonPrompt);
+
             } else if (selection->numberOfLeps() == 3) {
+                std::vector<double> scores = mva_ML->scoreEvent();
+
                 fillVec = fourTopHists::fillAllHists(true, selection);
+                fillVec.insert(fillVec.end(), scores.begin(), scores.end());
+
                 TriLManager->fillHistograms(fillVec, currentEvent->weight(), nonPrompt);
+
+                std::vector<std::pair<double, double>> fillVec2D = mva_ML->fill2DVector();
+                TriLManager->fill2DHistograms(fillVec2D, currentEvent->weight(), nonPrompt);
+
+                std::pair<MVAClasses, double> classAndScore = mva_ML->getClassAndScore();                
+                TriLManager->fillSingleHistogram(mlPosMVA + classAndScore.first, classAndScore.second, currentEvent->weight(), nonPrompt);
+                
             } else {
+                std::vector<double> scores = mva_ML->scoreEvent();
+
                 fillVec = fourTopHists::fillAllHists(true, selection, true);
+                fillVec.insert(fillVec.end(), scores.begin(), scores.end());
+
                 FourLManager->fillHistograms(fillVec, currentEvent->weight(), nonPrompt);
+
+                std::vector<std::pair<double, double>> fillVec2D = mva_ML->fill2DVector();
+                FourLManager->fill2DHistograms(fillVec2D, currentEvent->weight(), nonPrompt);
+
+                std::pair<MVAClasses, double> classAndScore = mva_ML->getClassAndScore();                
+                FourLManager->fillSingleHistogram(fourlPosMVA + classAndScore.first, classAndScore.second, currentEvent->weight(), nonPrompt);
+                
             }
 
         }
