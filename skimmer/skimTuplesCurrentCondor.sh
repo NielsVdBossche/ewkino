24c3ed7068724d3ccbc5a07ecc5e02c0ab566f18
njovdnbo <nielsvdbossche@gmail.com>
2021-11-19 16:53:23 +0100

    forgot change

--- skimmer/skimTuplesCurrentCondor.sh
+++ skimmer/skimTuplesCurrentCondor.sh
@@ -1,1 +1,1 @@
-python skimTuples.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpreVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 24:00:00 TTWW
+python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpreVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 24:00:00 TTWW
