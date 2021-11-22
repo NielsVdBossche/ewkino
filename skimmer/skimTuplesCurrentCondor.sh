cab820a65ba47263e056e71640b5a491bc319689
njovdnbo <nielsvdbossche@gmail.com>
2021-11-22 18:12:20 +0100

    other skimmer setup

--- skimmer/skimTuplesCurrentCondor.sh
+++ skimmer/skimTuplesCurrentCondor.sh
@@ -1,30 +1,5 @@
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpreVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTWW
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpostVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTWW
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2018_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTWW
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2017_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTWW
-
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpreVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTWH
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpostVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTWH
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2018_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTWH
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2017_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTWH
-
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpreVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTWZ
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpostVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTWZ
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2018_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTWZ
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2017_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTWZ
-
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpreVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTZZ
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpostVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTZZ
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2018_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTZZ
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2017_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTZZ
-
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpreVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTHH
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpostVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTHH
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2018_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTHH
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2017_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTHH
-
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpreVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTZH
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpostVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTZH
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2018_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTZH
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2017_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTZH
+#python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpreVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTWW
+python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2016_ULpostVFP /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTTo
+python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2018_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTTo
+python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrino 2017_UL /pnfs/iihe/cms/store/user/nivanden/skims/rawSkims light_SSdilepton_or_trilep 100 TTTo
 
