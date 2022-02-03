d819fcac00c302c992014c7e9aed54a69452e638
NielsVdBossche <nielsvdbossche@gmail.com>
2022-02-03 16:23:39 +0100

    added training for lean selection again

---
+++ MVATraining/trainMVALean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /user/nivanden/CMSSW_10_6_20/src
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+eval `scram runtime -sh`
+export X509_USER_PROXY=/user/$USER/x509up_u$(id -u $USER) 
+
+cd /user/nivanden/ewkino/MVATraining/
+
+./trainMVAMethodsExec sampleLists/DLTest.txt DL_tree_loose DL_BDT no
+./trainMVAMethodsExec sampleLists/DLTest.txt ML_tree_loose ML_BDT no
