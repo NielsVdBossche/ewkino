afaccce209359ff9a67c10262c5d5f71a4a12180
NielsVdBossche <nielsvdbossche@gmail.com>
2022-02-14 15:11:12 +0100

    solved problems in merging

--- fileManipulations/CombineOutput/main.cc
+++ fileManipulations/CombineOutput/main.cc
@@ -5,37 +5,47 @@
 #include <string>
 
 void makeCopyNest(TFile* outputFile, TFile* inputFile, std::string currentLoc) {
-    inputFile->cd(currentLoc.c_str());
+    inputFile->cd();
+    if (! gDirectory->cd(currentLoc.c_str())) return;
     TList* objects = gDirectory->GetListOfKeys();
     std::cout << currentLoc << std::endl;
 
     for(const auto&& obj: *objects) {
-        outputFile->cd(currentLoc.c_str());
+        
+        outputFile->cd();
+        gDirectory->cd(currentLoc.c_str());
 
         // if folder, do this
-        if (std::string(((TKey*) obj)->GetClassName()) == "TH1D" || std::string(((TKey*) obj)->GetClassName()) == "TH2D") {
+        if (std::string(((TKey*) obj)->GetClassName()) == "TH1D" || std::string(((TKey*) obj)->GetClassName()) == "TH1F" || std::string(((TKey*) obj)->GetClassName()) == "TH2D") {
             TH1* finalHist;
+            TH1* interHist = nullptr;
 
             if (gDirectory->GetListOfKeys()->Contains(obj->GetName())) {
                 gDirectory->GetObject(obj->GetName(), finalHist);
                 
-                TH1* interHist;
-                inputFile->cd(currentLoc.c_str());
+                inputFile->cd();
+                gDirectory->cd(currentLoc.c_str());
                 gDirectory->GetObject(obj->GetName(), interHist);
                 finalHist->Add(interHist);
             } else {
-                inputFile->cd(currentLoc.c_str());
+                inputFile->cd();
+                gDirectory->cd(currentLoc.c_str());
                 gDirectory->GetObject(obj->GetName(), finalHist);
             }
-            outputFile->cd(currentLoc.c_str());
+            outputFile->cd();
+            gDirectory->cd(currentLoc.c_str());
 
             finalHist->Write(finalHist->GetName(), TObject::kOverwrite);
+
+            delete finalHist;
+            if (interHist) delete interHist;
         } else {
             // check if it exists
             if (! gDirectory->GetDirectory(obj->GetName())) gDirectory->mkdir(obj->GetName());
             makeCopyNest(outputFile, inputFile, currentLoc + "/" + obj->GetName());
         }
     }
+    delete objects;
 }
 
 
@@ -44,7 +54,7 @@ int main(int argc, char const *argv[]) {
     First input is outputfile
     Others are inputfiles
     */
-    
+    TH1::AddDirectory(false);
     std::vector< std::string > argvStr( &argv[0], &argv[0] + argc );
 
     size_t splitPos = argvStr[1].find_last_of("_");
