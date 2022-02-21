8b0103cc7ebd3940c68f850af05f96e41854030a
NielsVdBossche <nielsvdbossche@gmail.com>
2022-02-21 14:31:19 +0100

    added python script to check if all datafiles were skimemd

---
+++ fileManipulations/CheckCompleteness/checkCompleteness.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import numpy as np
+import glob
+
+def missing_elements(L):
+    start, end = L[0], L[-1]
+    return sorted(set(range(start, end + 1)).difference(L))
+
+
+if __name__ == '__main__':
+    dir = "/home/njovdnbo/Documents/EPR/LumiBias/"# sys.argv[1]
+    dataset = "VDM"# sys.argv[2]
+
+    #### ask a dataset as argument
+    # list all folders matching this name in the dir
+    out = glob.glob(dir + "*" + dataset + "*")
+
+    print(out)
+
+    for subdir in out:
+        files = glob.glob(subdir + "/*.root")
+        # list all folders for this dataset and check  if there is continuity in the numbering
+        integers = np.zeros(len(files), dtype=np.int64)
+
+        for i, file in enumerate(files):
+            print(file[file.rfind('_'):-5])
+            # integers[i] = int(file[:-5])
+
+            
+
+        
+
+    
+
