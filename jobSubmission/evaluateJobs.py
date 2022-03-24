72672cb69f10f48cd56ed8792416ffa0fdfa7177
NielsVdBossche <nielsvdbossche@gmail.com>
2022-03-24 11:44:45 +0100

    evaluator for condorjobs

---
+++ jobSubmission/evaluateJobs.py
@@ -0,0 +1,27 @@
+import sys
+import glob
+
+base_dir = "/user/nivanden/condor/logs/"
+
+if __name__ == "__main__":
+    jobId = sys.argv[1]
+
+    # list all files with job id in /user/nivanden/condor/logs
+    # loop files until code 005 for termination and check for normal termination
+    logFiles = glob.glob(base_dir + "*" + jobId + "*")
+
+    for log in logFiles:
+        with open(log, 'r') as f:
+            for line in f:
+                if (line[:2] != "00"): continue
+
+                splitLine = line.split()
+
+                if (splitLine[0] == "005"):
+                    print("job " + log + " terminated with code 005. Should be nominal")
+                if (splitLine[0] == "009"):
+                    print("job " + log + " aborted")
+                if not any(splitLine[0] in id for id in ["000", "001", "002", "003"]):
+                    print("Check job " + log + ". Check manually.")
+                 
+
