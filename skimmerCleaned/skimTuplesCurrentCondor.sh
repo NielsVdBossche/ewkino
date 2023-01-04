f5d1a729ecf74fa719aad215743b76a1dc8a0b40
NielsVdBossche <niels.vandenbossche@ugent.be>
2023-01-04 22:57:40 +0100

    useless commit

--- skimmerCleaned/skimTuplesCurrentCondor.sh
+++ skimmerCleaned/skimTuplesCurrentCondor.sh
@@ -1,3 +1,4 @@
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/lwezenbe/heavyNeutrino/ singlelepton_data_UL2017V2 /pnfs/iihe/cms/store/user/nivanden/skims_v5/rawSkims/ lightDileptonSkim 150
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/lwezenbe/heavyNeutrino/ singlelepton_data_UL2016V2 /pnfs/iihe/cms/store/user/nivanden/skims_v5/rawSkims/ lightDileptonSkim 150
-python skimTuplesCondor.py /pnfs/iihe/cms/store/user/lwezenbe/heavyNeutrino/ singlelepton_data_UL2018V2 /pnfs/iihe/cms/store/user/nivanden/skims_v5/rawSkims/ lightDileptonSkim 150
+python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrinoGenPart/ MC_2016_ULpostVFP /pnfs/iihe/cms/store/user/nivanden/skims_v6/rawSkims/ lightDileptonSkim 150 WZJJ_EWK
+python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrinoGenPart/ MC_2016_ULpreVFP /pnfs/iihe/cms/store/user/nivanden/skims_v6/rawSkims/ lightDileptonSkim 150 WZJJ_EWK
+python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrinoGenPart/ MC_2017 /pnfs/iihe/cms/store/user/nivanden/skims_v6/rawSkims/ lightDileptonSkim 150 WZJJ_EWK
+python skimTuplesCondor.py /pnfs/iihe/cms/store/user/nivanden/heavyNeutrinoGenPart/ MC_2018 /pnfs/iihe/cms/store/user/nivanden/skims_v6/rawSkims/ lightDileptonSkim 150 WZJJ_EWK
