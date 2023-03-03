0dc794600cc942f01133bf65968a24d52ca40f17
LukaLambrecht <Luka.Lambrecht@UGent.be>
2023-03-03 18:04:46 +0100

    add triggers and MET filters

--- Event/src/TriggerInfo.cc
+++ Event/src/TriggerInfo.cc
@@ -8,8 +8,10 @@
 #include "../../Tools/interface/stringTools.h"
 
 
-//remove leading _ from trigger and filter names 
+// internal helper functions
+
 std::string cleanName( const std::string& name ){
+    // name cleaning
     std::string ret( name );
     if( stringTools::stringStartsWith( name, "_" ) ){
         ret.erase(0, 1);
@@ -17,6 +19,46 @@ std::string cleanName( const std::string& name ){
     return ret;
 }
 
+
+bool passTriggerOrFilter(
+    const std::map< std::string, bool >& decisionMap,
+    const std::string& name ){
+    // check if a trigger of filter is passed
+    auto decisionIt = decisionMap.find( name );
+    // throw error if non-existing trigger or MET filter is requested
+    if( decisionIt == decisionMap.cend() ){
+	std::string msg = "ERROR in TriggerInfo::passTriggerOrFilter:";
+	msg += " requested trigger or MET filter '" + name + "' does not exist.";
+        throw std::invalid_argument(msg);
+	//std::cout << msg << std::endl;
+    }
+    return (*decisionIt).second;
+}
+
+
+bool passAtLeastOneTrigger(
+    const std::map<std::string, bool>& decisionMap,
+    const std::vector<std::string>& names ){
+    // check if at least one trigger is passed
+    for( std::string name : names ){
+        if( passTriggerOrFilter(decisionMap,name) ) return true;
+    }
+    return false;
+}
+
+
+bool passAllTriggers(
+    const std::map<std::string, bool>& decisionMap,
+    const std::vector<std::string>& names ){
+    // check if at least one trigger is passed
+    for( std::string name : names ){
+        if( !passTriggerOrFilter(decisionMap,name) ) return false;
+    }
+    return true;
+}
+
+
+// constructor
 
 TriggerInfo::TriggerInfo( const TreeReader& treeReader,
 			  const bool readIndividualTriggers, 
@@ -31,18 +73,142 @@ TriggerInfo::TriggerInfo( const TreeReader& treeReader,
             individualMetFilterMap.insert( { cleanName( filter.first ), filter.second } );
         }   
     }
-}
+    // composite trigger definitions
+    // definition copied from ntuplizer
+    // (see here: https://github.com/GhentAnalysis/heavyNeutrino/blob/
+    //  UL_master/multilep/src/TriggerAnalyzer.cc)
+    std::map<std::string, std::vector<std::string>> triggerNames;
+    if( treeReader.is2018() ){
+	triggerNames["passTrigger_FR"] = {"HLT_Mu3_PFJet40", "HLT_Mu8", "HLT_Mu17", "HLT_Mu20", 
+					  "HLT_Mu27", "HLT_Ele8_CaloIdM_TrackIdM_PFJet30", 
+					  "HLT_Ele17_CaloIdM_TrackIdM_PFJet30", 
+					  "HLT_Ele23_CaloIdM_TrackIdM_PFJet30"};
+	triggerNames["passTrigger_FR_iso"] = {"HLT_Mu8_TrkIsoVVL", "HLT_Mu17_TrkIsoVVL", 
+					      "HLT_Ele8_CaloIdL_TrackIdL_IsoVL_PFJet30", 
+					      "HLT_Ele12_CaloIdL_TrackIdL_IsoVL_PFJet30", 
+					      //"HLT_Ele17_CaloIdL_TrackIdL_IsoVL_PFJet30", 
+					      "HLT_Ele23_CaloIdL_TrackIdL_IsoVL_PFJet30"};
+	triggerNames["passTrigger_m"] = {"HLT_IsoMu24", "HLT_IsoMu27", "HLT_Mu50", "HLT_OldMu100", "HLT_TkMu100"};
+	triggerNames["passTrigger_e"]   = {"HLT_Ele32_WPTight_Gsf", "HLT_Ele115_CaloIdVT_GsfTrkIdT", "HLT_Photon200"};
+	triggerNames["passTrigger_mm"]  = {"HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8", "HLT_Mu37_TkMu27"};
+	triggerNames["passTrigger_em"]  = {"HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ", 
+					   "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ",
+					   "HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ", 
+					   "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL",
+					   "HLT_Mu27_Ele37_CaloIdL_MW", "HLT_Mu37_Ele27_CaloIdL_MW"};
+	triggerNames["passTrigger_ee"]  = {"HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL", "HLT_DoubleEle25_CaloIdL_MW"};
+	triggerNames["passTrigger_mmm"] = {"HLT_TripleMu_10_5_5_DZ",
+					    //"HLT_TripleMu_5_3_3_Mass3p8to60_DZ",
+					    "HLT_TripleMu_5_3_3_Mass3p8_DZ",
+					    //"TripleMu_12_10_5",
+					    "HLT_TripleMu_12_10_5"};
+	triggerNames["passTrigger_emm"] = {"HLT_DiMu9_Ele9_CaloIdL_TrackIdL_DZ"};
+	triggerNames["passTrigger_eem"] = {"HLT_Mu8_DiEle12_CaloIdL_TrackIdL"};
+	triggerNames["passTrigger_eee"] = {"HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL"};
+	triggerNames["passTrigger_ref"] = {"HLT_CaloMET350_HBHECleaned", "HLT_CaloJet500_NoJetID", "HLT_AK8PFJet500", "HLT_AK8PFJet400_TrimMass30",
+                                   "HLT_DiJet110_35_Mjj650_PFMET110", "HLT_PFHT800_PFMET75_PFMHT75_IDTight", "HLT_PFHT700_PFMET85_PFMHT85_IDTight",
+                                   "HLT_PFHT500_PFMET100_PFMHT100_IDTight", "HLT_PFHT1050", "HLT_PFJet500", "HLT_PFMET120_PFMHT120_IDTight", 
+                                   "HLT_PFMET250_HBHECleaned", "HLT_PFMET200_HBHE_BeamHaloCleaned", "HLT_PFMETTypeOne140_PFMHT140_IDTight",
+                                   "HLT_PFMETTypeOne200_HBHE_BeamHaloCleaned", "HLT_TripleJet110_35_35_Mjj650_PFMET110"};
 
-
-bool passTriggerOrFilter( const std::map< std::string, bool >& decisionMap,  const std::string& name ){
-    auto decisionIt = decisionMap.find( name );
-
-    //throw error if non-existing trigger or MET filter is requested
-    if( decisionIt == decisionMap.cend() ){
-        throw std::invalid_argument( "Requested trigger or MET filter '" + name + "' does not exist." );
+    } else if( treeReader.is2017() ){
+	triggerNames["passTrigger_FR"] = {"HLT_Mu3_PFJet40", "HLT_Mu8", "HLT_Mu17", "HLT_Mu20", "HLT_Mu27", 
+					    "HLT_Ele8_CaloIdM_TrackIdM_PFJet30", "HLT_Ele17_CaloIdM_TrackIdM_PFJet30", 
+					    "HLT_Ele23_CaloIdM_TrackIdM_PFJet30"};
+	triggerNames["passTrigger_FR_iso"] = {"HLT_Mu8_TrkIsoVVL", "HLT_Mu17_TrkIsoVVL",
+					    "HLT_Ele8_CaloIdL_TrackIdL_IsoVL_PFJet30", 
+					    "HLT_Ele12_CaloIdL_TrackIdL_IsoVL_PFJet30",
+					    //"HLT_Ele17_CaloIdL_TrackIdL_IsoVL_PFJet30", 
+					    "HLT_Ele23_CaloIdL_TrackIdL_IsoVL_PFJet30"};
+	triggerNames["passTrigger_m"] = {"HLT_IsoMu24", "HLT_IsoMu24_eta2p1", "HLT_IsoMu27", "HLT_Mu50", "HLT_OldMu100", "HLT_TkMu100"};
+	triggerNames["passTrigger_e"] = {"HLT_Ele32_WPTight_Gsf", "HLT_Ele35_WPTight_Gsf", "HLT_Ele115_CaloIdVT_GsfTrkIdT", "HLT_Photon200"};
+	triggerNames["passTrigger_mm"] = {"HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL", "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ", "HLT_Mu37_TkMu27",
+					    "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8", "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8"};
+	triggerNames["passTrigger_em"] = {"HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ",
+					"HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL",
+					"HLT_Mu27_Ele37_CaloIdL_MW", "HLT_Mu37_Ele27_CaloIdL_MW"};
+	triggerNames["passTrigger_ee"] = {"HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL", "HLT_DoubleEle33_CaloIdL_MW"};
+	triggerNames["passTrigger_mmm"] = {"HLT_TripleMu_10_5_5_DZ",
+					    //"HLT_TripleMu_5_3_3_Mass3p8to60_DZ",
+					    "HLT_TripleMu_5_3_3_Mass3p8_DZ",
+					    //"TripleMu_12_10_5",
+					    "HLT_TripleMu_12_10_5"};
+	triggerNames["passTrigger_emm"] = {"HLT_DiMu9_Ele9_CaloIdL_TrackIdL_DZ"};
+	triggerNames["passTrigger_eem"] = {"HLT_Mu8_DiEle12_CaloIdL_TrackIdL"};
+	triggerNames["passTrigger_eee"] = {"HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL"};
+	triggerNames["passTrigger_ref"] = {"HLT_PFJet500", "HLT_PFMET140_PFMHT140_IDTight", "HLT_PFHT500_PFMET100_PFMHT100_IDTight",
+					    "HLT_PFHT700_PFMET85_PFMHT85_IDTight", "HLT_PFHT800_PFMET75_PFMHT75_IDTight", "HLT_CaloJet500_NoJetID",
+					    "HLT_AK8PFJet500"};
+  
     } else {
-        return (*decisionIt).second;
+	triggerNames["passTrigger_FR"] = {"HLT_Mu3_PFJet40", "HLT_Mu8", "HLT_Mu17", "HLT_Mu20", "HLT_Mu27", 
+					    "HLT_Ele8_CaloIdM_TrackIdM_PFJet30", "HLT_Ele12_CaloIdM_TrackIdM_PFJet30", 
+					    "HLT_Ele17_CaloIdM_TrackIdM_PFJet30", "HLT_Ele23_CaloIdM_TrackIdM_PFJet30"};
+	triggerNames["passTrigger_FR_iso"] = {"HLT_Mu8_TrkIsoVVL", 
+					    "HLT_Mu17_TrkIsoVVL", 
+					    "HLT_Ele8_CaloIdL_TrackIdL_IsoVL_PFJet30", 
+					    "HLT_Ele12_CaloIdL_TrackIdL_IsoVL_PFJet30", 
+					    //"HLT_Ele17_CaloIdL_TrackIdL_IsoVL_PFJet30", 
+					    "HLT_Ele23_CaloIdL_TrackIdL_IsoVL_PFJet30"};
+	triggerNames["passTrigger_m"] = {"HLT_IsoMu24", "HLT_IsoTkMu24", "HLT_Mu50", "HLT_TkMu50", "HLT_Mu45_eta2p1"};
+	triggerNames["passTrigger_e"] = {"HLT_Ele27_WPTight_Gsf", "HLT_Ele105_CaloIdVT_GsfTrkIdT", "HLT_Ele115_CaloIdVT_GsfTrkIdT", "HLT_Photon175"};
+	triggerNames["passTrigger_mm"] = {"HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL", "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ",
+					    "HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL", "HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ",
+					    "HLT_TkMu17_TrkIsoVVL_TkMu8_TrkIsoVVL", "HLT_TkMu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ",
+					    "HLT_Mu30_TkMu11"};
+	triggerNames["passTrigger_em"] = {"HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL", "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ",
+					    "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL", "HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ",
+					    "HLT_Mu30_Ele30_CaloIdL_GsfTrkIdVL", "HLT_Mu33_Ele33_CaloIdL_GsfTrkIdVL"};
+	triggerNames["passTrigger_ee"] = {"HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ", "HLT_DoubleEle33_CaloIdL_GsfTrkIdVL", 
+					    "HLT_DoubleEle33_CaloIdL_GsfTrkIdVL_MW"};
+	triggerNames["passTrigger_et"] = {"HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFtau20_SingleL1", "HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20",
+					    "HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau30"};
+	triggerNames["passTrigger_mmm"] = {"HLT_TripleMu_12_10_5"};
+	triggerNames["passTrigger_emm"] = {"HLT_DiMu9_Ele9_CaloIdL_TrackIdL"};
+	triggerNames["passTrigger_eem"] = {"HLT_Mu8_DiEle12_CaloIdL_TrackIdL"};
+	triggerNames["passTrigger_eee"] = {"HLT_Ele16_Ele12_Ele8_CaloIdL_TrackIdL"};
+	triggerNames["passTrigger_ref"] = {"HLT_MET200", "HLT_PFMET300", "HLT_PFMET170_HBHECleaned", "HLT_PFMET120_PFMHT120_IDTight",
+					    "HLT_PFHT300_PFMET110", "HLT_PFHT350_DiPFJetAve90_PFAlphaT0p53", "HLT_PFHT400_DiPFJetAve90_PFAlphaT0p52",
+					    "HLT_PFHT400_SixJet30_DoubleBTagCSV_p056", "HLT_PFHT900", "HLT_PFHT650_WideJetMJJ900DEtaJJ1p5", 
+					    "HLT_CaloJet500_NoJetID"};
     }
+    // check if all triggers exist
+    for(auto const& el : triggerNames){
+	for(std::string triggerName : el.second){
+	   if( treeReader._triggerMap.find(triggerName)==treeReader._triggerMap.end()){
+		std::string msg = "ERROR in TriggerInfo constructor:";
+		msg += " trigger " + triggerName + " not found in TreeReader.";
+		throw std::runtime_error(msg);
+		//std::cerr << msg << std::endl;
+	    }
+	}
+    }
+    // set the composite trigger variables
+    _passTriggers_e = passAtLeastOneTrigger( treeReader._triggerMap, triggerNames["passTrigger_e"] );
+    _passTriggers_m = passAtLeastOneTrigger( treeReader._triggerMap, triggerNames["passTrigger_m"] );
+    _passTriggers_ee = passAtLeastOneTrigger( treeReader._triggerMap, triggerNames["passTrigger_ee"] );
+    _passTriggers_em = passAtLeastOneTrigger( treeReader._triggerMap, triggerNames["passTrigger_em"] );
+    _passTriggers_mm = passAtLeastOneTrigger( treeReader._triggerMap, triggerNames["passTrigger_mm"] );
+    _passTriggers_eee = passAtLeastOneTrigger( treeReader._triggerMap, triggerNames["passTrigger_eee"] );
+    _passTriggers_eem = passAtLeastOneTrigger( treeReader._triggerMap, triggerNames["passTrigger_eem"] );
+    _passTriggers_emm = passAtLeastOneTrigger( treeReader._triggerMap, triggerNames["passTrigger_emm"] );
+    _passTriggers_mmm = passAtLeastOneTrigger( treeReader._triggerMap, triggerNames["passTrigger_mmm"] );
+    _passTriggers_FR = passAtLeastOneTrigger( treeReader._triggerMap, triggerNames["passTrigger_FR"] );
+    _passTriggers_FR_iso = passAtLeastOneTrigger( treeReader._triggerMap, triggerNames["passTrigger_FR_iso"] );
+    _passTriggers_ref = passAtLeastOneTrigger( treeReader._triggerMap, triggerNames["passTrigger_ref"] );
+    // composite MET filter definitions
+    // can use builtin "Flag_METFilters" in nanoAOD!
+    std::vector<std::string> metNames = {"Flag_METFilters"};
+    // check if all MET filters exist
+    for(std::string metName : metNames){
+        if( treeReader._METFilterMap.find(metName)==treeReader._METFilterMap.end()){
+            std::string msg = "ERROR in TriggerInfo constructor:";
+            msg += " MET filter " + metName + " not found in TreeReader.";
+            throw std::runtime_error(msg);
+        }
+    }
+    // set the composite MET filter variables
+    _passMetFilters = passAllTriggers( treeReader._METFilterMap, metNames );
 }
 
 
