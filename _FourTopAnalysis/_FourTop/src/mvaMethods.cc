ffdbbe71e345018fe92b182faf5ea248b12abab4
NielsVdBossche <nielsvdbossche@gmail.com>
2023-03-14 21:57:05 +0100

    more missing entries

--- _FourTopAnalysis/_FourTop/src/mvaMethods.cc
+++ _FourTopAnalysis/_FourTop/src/mvaMethods.cc
@@ -130,12 +130,12 @@ void FourTop::fillMVAVariables(bool isML) {
     massSecTop = topReco->getSecondBestRecoTop().first;
     massSecTopW = topReco->getSecondBestRecoTop().second;
 
-    mtLeadLepMET = mt(*lightLeps->at(0), selection->getEvent()->met());
-    mtSubLeadLepMET = mt(*lightLeps->at(1), selection->getEvent()->met());
-    m2ll = mt2::mt2Alt(*lightLeps->at(0), *lightLeps->at(1), selection->getEvent()->met());
+    mtLeadLepMET = mt(*lightLeps->at(0), selection->GetFullMET());
+    mtSubLeadLepMET = mt(*lightLeps->at(1), selection->GetFullMET());
+    m2ll = mt2::mt2Alt(*lightLeps->at(0), *lightLeps->at(1), selection->GetFullMET());
 
-    m2bb = (n_b_loose >= 2 ? mt2::mt2bb((*bJets)[0], (*bJets)[1], (*lightLeps)[0], (*lightLeps)[1], selection->getEvent()->met()) : -1);
-    m2lblb = (n_b_loose >= 2 ? mt2::mt2lblb((*bJets)[0], (*bJets)[1], (*lightLeps)[0], (*lightLeps)[1], selection->getEvent()->met()) : -1);
+    m2bb = (n_b_loose >= 2 ? mt2::mt2bb((*bJets)[0], (*bJets)[1], (*lightLeps)[0], (*lightLeps)[1], selection->GetFullMET()) : -1);
+    m2lblb = (n_b_loose >= 2 ? mt2::mt2lblb((*bJets)[0], (*bJets)[1], (*lightLeps)[0], (*lightLeps)[1], selection->GetFullMET()) : -1);
 
     n_mu = selection->getMediumLepCol()->numberOfMuons();
     chargeSum = selection->getMediumLepCol()->sumCharges();
